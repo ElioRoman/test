b086a43a10dd55d5f1f535fa536fd6363ec34773
roman.kolomiiets <roman.kolomiets@strikersoft.com>
2023-08-19 13:04:41 +0300

    add files

---
+++ src/UserCard.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import {
+  Avatar,
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  LinearProgress,
+  Link,
+  Typography,
+} from "@mui/material";
+import { Store } from "./store";
+import { observer } from "mobx-react-lite";
+
+const UserCard = observer(() => {
+  const { userData, errorMessage, isLoading } = Store;
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        m: 1,
+      }}
+    >
+      {isLoading ? (
+        <Box sx={{ width: "100%" }}>
+          <LinearProgress />
+        </Box>
+      ) : null}
+
+      {userData !== null ? (
+        <Card>
+          <CardHeader
+            avatar={<Avatar alt="User Avatar" src={userData.avatar_url} />}
+            title={userData.name}
+            subheader={userData.bio}
+          />
+          <CardContent>
+            <Link target="_blank" href={userData.url} underline="none">
+              Link
+            </Link>
+          </CardContent>
+        </Card>
+      ) : null}
+
+      <Typography color={"red"}>{errorMessage && errorMessage}</Typography>
+    </Box>
+  );
+});
+
+export default UserCard;
---
+++ src/store/index.tsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { makeAutoObservable, flow } from "mobx";
+
+interface IUserData {
+  name: string;
+  bio: string;
+  avatar_url: string;
+  url: string;
+}
+
+class Data {
+  userData: IUserData | null = null;
+  isLoading: boolean = false;
+  errorMessage: string = "";
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  getData = flow(function* (this: Data, name) {
+    this.isLoading = true;
+    this.userData = null;
+    this.errorMessage = "";
+    try {
+      const { data } = yield axios.get(`https://api.github.com/users/${name}`);
+      this.userData = data;
+      this.isLoading = false;
+    } catch (error: any) {
+      this.errorMessage = error.response?.data?.message || error.message;
+      this.isLoading = false;
+      this.userData = null;
+    } finally {
+      this.isLoading = false;
+    }
+  });
+}
+export const Store = new Data();
